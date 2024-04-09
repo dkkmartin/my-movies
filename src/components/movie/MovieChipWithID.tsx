1f174719056e59e2b3dfc9c6f16521dfb9f68cc8
Martin <martinbruun1995@gmail.com>
2024-04-09 20:43:03 +0200

    Add MovieChipWithID component

---
+++ src/components/movie/MovieChipWithID.tsx
@@ -0,0 +1,96 @@
+'use client'
+
+import { Chip } from '@nextui-org/react'
+
+type Chip = number
+const genres_ids = [
+  {
+    id: 28,
+    name: 'Action',
+  },
+  {
+    id: 12,
+    name: 'Adventure',
+  },
+  {
+    id: 16,
+    name: 'Animation',
+  },
+  {
+    id: 35,
+    name: 'Comedy',
+  },
+  {
+    id: 80,
+    name: 'Crime',
+  },
+  {
+    id: 99,
+    name: 'Documentary',
+  },
+  {
+    id: 18,
+    name: 'Drama',
+  },
+  {
+    id: 10751,
+    name: 'Family',
+  },
+  {
+    id: 14,
+    name: 'Fantasy',
+  },
+  {
+    id: 36,
+    name: 'History',
+  },
+  {
+    id: 27,
+    name: 'Horror',
+  },
+  {
+    id: 10402,
+    name: 'Music',
+  },
+  {
+    id: 9648,
+    name: 'Mystery',
+  },
+  {
+    id: 10749,
+    name: 'Romance',
+  },
+  {
+    id: 878,
+    name: 'Science Fiction',
+  },
+  {
+    id: 10770,
+    name: 'TV Movie',
+  },
+  {
+    id: 53,
+    name: 'Thriller',
+  },
+  {
+    id: 10752,
+    name: 'War',
+  },
+  {
+    id: 37,
+    name: 'Western',
+  },
+]
+
+export default function MovieChipWithID({ chip }: { chip: Chip }) {
+  const genre = genres_ids.find((genre) => genre.id === chip)
+  const genreName = genre ? genre.name : chip
+
+  return (
+    <>
+      <Chip className="h-5 text-xs py-2" size="sm" color="primary">
+        {genreName}
+      </Chip>
+    </>
+  )
+}
