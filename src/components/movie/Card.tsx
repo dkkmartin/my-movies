d0649df752540b1b48363cff5d9fef803c3a1d77
Martin <martinbruun1995@gmail.com>
2024-04-09 20:43:10 +0200

    Update movie Card and CardBig components

--- src/components/movie/Card.tsx
+++ src/components/movie/Card.tsx
@@ -1,33 +1,47 @@
 'use client'
 
 import { Card, CardBody, CardFooter, Image } from '@nextui-org/react'
+import { useRouter } from 'next/navigation'
 
 interface Movie {
-  image: string
+  id: number
+  poster_path: string
   title: string
-  rating: string
+  vote_average: number
 }
 
 export default function MovieCard({ movie }: { movie: Movie }) {
+  const router = useRouter()
   return (
     <Card
-      className="w-[180px]"
+      className="w-[180px] h-full"
       shadow="sm"
       isPressable
-      onPress={() => console.log('item pressed')}
+      onPress={() => {
+        router.push(`/movie/${movie.id}`)
+      }}
     >
-      <CardBody>
+      <CardBody className="flex-grow-0">
         <Image
           shadow="sm"
           radius="lg"
-          width="100%"
-          alt="spiderman"
-          src={movie.image}
+          width={180}
+          alt={movie.title}
+          src={'https://image.tmdb.org/t/p/original/' + movie.poster_path}
         />
       </CardBody>
-      <CardFooter className="text-small justify-between flex flex-col">
-        <b className="text-pretty">{movie.title}</b>
-        <p className="text-default-500">{movie.rating}</p>
+      <CardFooter className=" text-small flex flex-col items-start gap-2">
+        <b className="text-start">{movie.title}</b>
+        <div className="flex gap-2 items-center">
+          <Image
+            src="/MaterialSymbolsKidStar.svg"
+            width={20}
+            alt="Star"
+          ></Image>
+          <p className="text-default-500">
+            {movie.vote_average.toFixed(1) + '/10 IMDb'}
+          </p>
+        </div>
       </CardFooter>
     </Card>
   )
--- src/components/movie/CardBig.tsx
+++ src/components/movie/CardBig.tsx
@@ -1,42 +1,77 @@
 'use client'
 
-import { Card, CardBody, Chip, Image } from '@nextui-org/react'
-import MovieChip from './MovieChip'
+import { Card, CardBody, Image } from '@nextui-org/react'
+import MovieChipWithID from './MovieChipWithID'
+import { useRouter } from 'next/navigation'
+import useSWR from 'swr'
 
 interface Movie {
-  image: string
+  id: number
+  poster_path: string
   title: string
-  rating: string
-  tags: string[]
-  duration: string
+  vote_average: number
+  genre_ids: number[]
 }
 
 export default function MovieCardBig({ movie }: { movie: Movie }) {
+  const router = useRouter()
+  const fetcher = (url: string) => fetch(url).then((res) => res.json())
+  const {
+    data: movieDuration,
+    error: movieDurationError,
+    isLoading: movieDurationIsLoading,
+  } = useSWR(`/api/movie/duration/${movie.id}`, fetcher, {
+    refreshInterval: 0,
+  })
+
+  const lengthConverter = (minutes: number) => {
+    const hours = Math.floor(minutes / 60)
+    const remainingMinutes = minutes % 60
+    return `${hours}h ${remainingMinutes}min`
+  }
+
   return (
     <Card
       className="w-full"
       shadow="sm"
       isPressable
-      onPress={() => console.log('item pressed')}
+      onPress={() => {
+        router.push(`/movie/${movie.id}`)
+      }}
     >
       <CardBody>
         <div className="flex gap-4">
           <Image
+            className="min-w-[120px]"
             shadow="sm"
             radius="lg"
-            width={100}
-            alt="spiderman"
-            src={movie.image}
+            width={120}
+            alt={movie.title}
+            src={'https://image.tmdb.org/t/p/original/' + movie.poster_path}
           />
           <div className="flex flex-col gap-2">
             <b className="text-pretty">{movie.title}</b>
-            <p className="text-default-500">{movie.rating}</p>
-            <div className="flex gap-2">
-              {movie.tags.map((chip, index) => (
-                <MovieChip key={index} chips={[chip]}></MovieChip>
+            <div className="flex gap-2 items-center">
+              <Image
+                src="/MaterialSymbolsKidStar.svg"
+                width={20}
+                alt="Star"
+              ></Image>
+              <p className="text-default-500">
+                {movie.vote_average.toFixed(1) + '/10 IMDb'}
+              </p>
+            </div>
+            <div className="flex gap-2 flex-wrap w-full">
+              {movie.genre_ids.map((chip, index) => (
+                <MovieChipWithID key={index} chip={chip}></MovieChipWithID>
               ))}
             </div>
-            <p>{movie.duration}</p>
+            <div className="flex gap-2 items-center">
+              <Image src="/MdiClockOutline.svg" width={20} alt="Star"></Image>
+              {movieDurationIsLoading ? null : (
+                <p>{lengthConverter(movieDuration && movieDuration.runtime)}</p>
+              )}
+            </div>
           </div>
         </div>
       </CardBody>
