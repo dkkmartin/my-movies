d70b4637ebd2e81ec477b3b002b5558ca985c1e9
Martin <martinbruun1995@gmail.com>
2024-04-11 13:53:13 +0200

    Add SVG component

---
+++ src/components/svg/StreamlineInterfaceTimeClockCircleClockLoadingMeasureTimeCircle.tsx
@@ -0,0 +1,26 @@
+import React, { SVGProps } from 'react'
+
+export function StreamlineInterfaceTimeClockCircleClockLoadingMeasureTimeCircle(
+  props: SVGProps<SVGSVGElement>
+) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1.3em"
+      height="1.3em"
+      viewBox="0 0 15 15"
+      {...props}
+    >
+      <g
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <circle cx="7" cy="7" r="6.5"></circle>
+        <path d="M7 4.5V7l2.54 2.96"></path>
+      </g>
+    </svg>
+  )
+}
+export default StreamlineInterfaceTimeClockCircleClockLoadingMeasureTimeCircle
